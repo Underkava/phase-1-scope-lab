188d71179c77286a56307c85153de123a65ca2aa
Samuel Kinyua <samwakash@gmail.com>
2024-07-04 13:20:39 -0700

    master

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+console.log(customerName); 
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+console.log(bestCustomer);
+
+function overwriteBestCustomer(){
+    bestCustomer= "maybe bob"
+}
+console.log(bestCustomer);
+
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'joe';
+
+        leastFavoriteCustomer = 'jay'; 
+        
+    }
+    
+console.log(leastFavoriteCustomer); 
